f75e148d8eef00fe6da8e9d0868ae1d5cd1b1688
Allen Pai <42284230+slacAdpai@users.noreply.github.com>
2020-02-13 07:21:39 -0800

    DEV: changed name of ioc to lowercase name

--- PLC_LFE_VAC/deploy_config.py
+++ PLC_LFE_VAC/deploy_config.py
@@ -1,4 +1,3 @@
-projects = ['PLC_LFE_VAC/PLC_LFE_VAC.tsproj']
-local_net_id = "198.129.119.34.1.1"
-# local_net_id = "172.17.26.17.1.1"
+projects = ['plc_lfe_vac/plc_lfe_vac.tsproj']
+local_net_id = "UNKNOWN"
 # TODO: check local_net_id above; it should match Windows
