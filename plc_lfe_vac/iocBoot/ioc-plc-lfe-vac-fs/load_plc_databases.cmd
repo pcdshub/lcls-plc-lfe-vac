93179cae8ea94309e801017f78495d5b47dd8849
Allen Pai <adpai@slac.stanford.edu>
2021-01-13 16:14:44 -0800

    MNT: Update IOC to 0.4.0.

--- plc_lfe_vac/iocBoot/ioc-plc-lfe-vac-fs/load_plc_databases.cmd
+++ plc_lfe_vac/iocBoot/ioc-plc-lfe-vac-fs/load_plc_databases.cmd
@@ -1,1 +1,3 @@
-dbLoadRecords("plc_lfe_vac_fs.db", "PORT=ASYN_PLC,PREFIX=PREFIX:,IOCNAME=$(IOC),IOC=$(IOC)")
+# Total records: 1005
+callbackSetQueueSize(4010)
+dbLoadRecords("plc_lfe_vac_fs.db", "PORT=ASYN_PLC,PREFIX=PLC:LFE:VAC:,IOCNAME=$(IOC),IOC=$(IOC)")
--- plc_lfe_vac/iocBoot/ioc-plc-lfe-vac/load_plc_databases.cmd
+++ plc_lfe_vac/iocBoot/ioc-plc-lfe-vac/load_plc_databases.cmd
@@ -1,1 +1,3 @@
-dbLoadRecords("plc_lfe_vac.db", "PORT=ASYN_PLC,PREFIX=PREFIX:,IOCNAME=$(IOC),IOC=$(IOC)")
+# Total records: 3930
+callbackSetQueueSize(9860)
+dbLoadRecords("plc_lfe_vac.db", "PORT=ASYN_PLC,PREFIX=PLC:LFE:VAC:,IOCNAME=$(IOC),IOC=$(IOC)")
